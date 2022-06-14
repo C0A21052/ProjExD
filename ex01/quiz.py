3428b5b28264ab8ee2a6d6b2dfcf90362702dfb3
tomotaka kobayashi <c0a2105259@edu.teu.ac.jp>
2022-06-14 16:15:51 +0900

    クイズ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,6 +1,7 @@
 
 
 
+from locale import ABDAY_1
 import random 
 import datetime
 
@@ -14,7 +15,14 @@ def shutudai(a):
 def kaito(a):
     A_list = [["ますお","マスオ"],["わかめ","ワカメ"],["甥","おい","甥っ子","おいっこ"]]
     ans = input("答えるんだ：")
-    if ans in A_list
+    if ans in A_list[a]:
         print("正解！！！")
     else:
         print("出直してこい")
+
+if __name__ == "__main__":
+    a1 = random.randint(0,2)
+    shutudai(a1)
+    kaito(a1)
+
+
