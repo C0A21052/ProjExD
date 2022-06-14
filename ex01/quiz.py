a8548a7a12c3bdf18174c934bb60132e4aff82ba
tomotaka kobayashi <c0a2105259@edu.teu.ac.jp>
2022-06-14 16:01:55 +0900

    クイズ

---
+++ ex01/quiz.py
@@ -0,0 +1,20 @@
+
+
+
+import random 
+import datetime
+
+def shutudai(a):
+    Q_list = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+    print = ("問題："+ "\n" + Q_list[a])
+    
+
+
+
+def kaito(a):
+    A_list = [["ますお","マスオ"],["わかめ","ワカメ"],["甥","おい","甥っ子","おいっこ"]]
+    ans = input("答えるんだ：")
+    if ans in A_list
+        print("正解！！！")
+    else:
+        print("出直してこい")
