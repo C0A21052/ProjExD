789af6f4d78e75e046e040b8ed5142215892769c
tomotaka kobayashi <c0a2105259@edu.teu.ac.jp>
2022-06-14 17:52:56 +0900

    game

---
+++ ex01/alpgame.py
@@ -0,0 +1,37 @@
+
+import random
+import datetime
+
+al = 5
+ak = 10
+aj = 2
+def main():
+    st = datetime.datetime.now()
+    for _ in range(al):
+        seikai = shutudai()
+        kaitou(seikai)
+    ed = datetime.datetime.now()
+    print(f"{(ed-st).seconds}秒かかりました")
+
+def shutudai():
+    alp_list = [chr(ord("a")+i) for i in range(26)]
+    all_char_lst = random.sample(alp_list, ak)
+    print(f"対象文字：{all_char_lst}")
+    abs_char_lst = random.sample(XXXX, YYYY)
+    print(f"欠損文字：{abs_char_lst}")
+    pre_char_lst = [ZZZZ]
+    print(f"表示文字：{pre_char_lst}")
+
+
+def kaitou(seikai):
+    num = int(input("欠損文字はいくつあるでしょうか？"))
+    if num != aj:
+        print("不正解です、またチャレンジしてね")
+        return 0
+    else:
+        print("正解です。それでは具体的に欠損文字を一つずつ入力してください")
+        for i  in range(aj):
+            c = input()
+
+
+
